dbc026661eb36d2c1f14381f9395c7d7705a5959
Jaylin <895350975@qq.com>
2019-04-15 22:57:10 +0800

    Init

---
+++ app/src/main/java/vip/hitsz/auto_auth/MainActivity.java
@@ -0,0 +1,22 @@
+package vip.hitsz.auto_auth;
+
+import android.app.*;
+import android.os.*;
+import android.widget.*;
+
+
+public class MainActivity extends Activity 
+{
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+        
+        IntentFilter filter = new IntentFilter();
+        filter.addAction(WifiManager.WIFI_STATE_CHANGED_ACTION);
+        filter.addAction(WifiManager.NETWORK_STATE_CHANGED_ACTION);
+        filter.addAction(ConnectivityManager.CONNECTIVITY_ACTION);
+        registerReceiver(new WifiReceiver(), filter);
+    }
+}
